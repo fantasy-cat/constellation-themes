ba3753dbf3582451deeefa1603b4d8e7d4c5b639
CreepWannabe <contact@creep.cat>
2021-10-28 16:53:42 +0200

    fixed js not working updated for latest version

--- pochita-menu/js/main.js
+++ pochita-menu/js/main.js
@@ -1,10 +1,39 @@
-import Controller from './Menu.js'
+class Controller {
+    sectionQuery = null
+    navQuery = null
+    sections = document.querySelectorAll('.section')
+    navigation = document.querySelectorAll('.nav-item')
+
+    doLoop() {
+        this.sections.forEach((section) => this.toggleDisplay(section))
+        this.navigation.forEach((nav) => this.toggleActive(nav))
+    }
+
+    toggleDisplay(section) {
+        section?.classList?.replace('visible', 'hidden')
+        this.sectionQuery?.classList?.replace('hidden', 'visible')
+    }
+
+    toggleActive(nav) {
+        nav?.classList?.replace('active', 'inactive')
+        this.navQuery?.classList?.replace('inactive', 'active')
+    }
+
+    constructor(sectionelement, navelement) {
+        this.sectionelement = sectionelement
+        this.navelement = navelement
+        this.sectionQuery = document.querySelector(sectionelement)
+        this.navQuery = document.querySelector(navelement)
+        this.doLoop()
+    }
+}
 
 const main = document.querySelector("#main-toggle")
 const humanizer = document.querySelector("#humanizer-toggle")
 const weapon = document.querySelector("#weapon-toggle")
 const triggerbot = document.querySelector("#triggerbot-toggle")
 const sound = document.querySelector("#sound-toggle")
+const tf2 = document.querySelector("#tf2-toggle")
 
 main.onclick = () => {
     new Controller('#main', '#main-toggle')
@@ -24,4 +53,8 @@ triggerbot.onclick = () => {
 
 sound.onclick = () => {
     new Controller('#sound', '#sound-toggle')
+}
+
+tf2.onclick = () => {
+    new Controller('#tf2', '#tf2-toggle')
 }
