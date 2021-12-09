0947d2451dac407de0a7429425f55b7f304897c9
CreepWannabe <contact@creep.cat>
2021-12-09 20:11:42 +0100

    updated to match latest software changes

--- pochita-menu/js/main.js
+++ pochita-menu/js/main.js
@@ -1,60 +1,56 @@
-class Controller {
-    sectionQuery = null
-    navQuery = null
-    sections = document.querySelectorAll('.section')
-    navigation = document.querySelectorAll('.nav-item')
-
-    doLoop() {
-        this.sections.forEach((section) => this.toggleDisplay(section))
-        this.navigation.forEach((nav) => this.toggleActive(nav))
+var DEFAULT_SECTION_VISIBLE = 'visible';
+var DEFAULT_SECTION_HIDDEN = 'hidden';
+var SECTION_ELEMENT = '.section';
+var DEFAULT_NAV_VISIBLE = 'active';
+var DEFAULT_NAV_HIDDEN = 'inactive';
+var NAV_ELEMENT = '.nav-item';
+var Controller = /** @class */ (function () {
+    function Controller(sectionQuery, navigatorQuery) {
+        this._section = Array.from(document.querySelectorAll(SECTION_ELEMENT));
+        this._navigator = Array.from(document.querySelectorAll(NAV_ELEMENT));
+        this._sectionQuery = document.querySelector(sectionQuery);
+        this._navigatorQuery = document.querySelector(navigatorQuery);
+        this._doLoop();
     }
-
-    toggleDisplay(section) {
-        section?.classList?.replace('visible', 'hidden')
-        this.sectionQuery?.classList?.replace('hidden', 'visible')
-    }
-
-    toggleActive(nav) {
-        nav?.classList?.replace('active', 'inactive')
-        this.navQuery?.classList?.replace('inactive', 'active')
-    }
-
-    constructor(sectionelement, navelement) {
-        this.sectionelement = sectionelement
-        this.navelement = navelement
-        this.sectionQuery = document.querySelector(sectionelement)
-        this.navQuery = document.querySelector(navelement)
-        this.doLoop()
-    }
-}
-
-const main = document.querySelector("#main-toggle")
-const humanizer = document.querySelector("#humanizer-toggle")
-const weapon = document.querySelector("#weapon-toggle")
-const triggerbot = document.querySelector("#triggerbot-toggle")
-const sound = document.querySelector("#sound-toggle")
-const tf2 = document.querySelector("#tf2-toggle")
-
-main.onclick = () => {
-    new Controller('#main', '#main-toggle')
-}
-
-humanizer.onclick = () => {
-    new Controller('#humanizer', '#humanizer-toggle')
-}
-
-weapon.onclick = () => {
-    new Controller('#weapon', '#weapon-toggle')
-}
-
-triggerbot.onclick = () => {
-    new Controller('#triggerbot', '#triggerbot-toggle')
-}
-
-sound.onclick = () => {
-    new Controller('#sound', '#sound-toggle')
-}
-
-tf2.onclick = () => {
-    new Controller('#tf2', '#tf2-toggle')
-}
+    Controller.prototype._doLoop = function () {
+        var _this = this;
+        var _a, _b;
+        (_a = this._section) === null || _a === void 0 ? void 0 : _a.forEach(function (item) { _this._toggleSections(item); });
+        (_b = this._navigator) === null || _b === void 0 ? void 0 : _b.forEach(function (item) { _this._toggleActive(item); });
+    };
+    Controller.prototype._toggleSections = function (item) {
+        var _a, _b, _c;
+        (_a = item === null || item === void 0 ? void 0 : item.classList) === null || _a === void 0 ? void 0 : _a.replace(DEFAULT_SECTION_VISIBLE, DEFAULT_SECTION_HIDDEN);
+        (_c = (_b = this._sectionQuery) === null || _b === void 0 ? void 0 : _b.classList) === null || _c === void 0 ? void 0 : _c.replace(DEFAULT_SECTION_HIDDEN, DEFAULT_SECTION_VISIBLE);
+    };
+    Controller.prototype._toggleActive = function (item) {
+        var _a, _b, _c;
+        (_a = item === null || item === void 0 ? void 0 : item.classList) === null || _a === void 0 ? void 0 : _a.replace(DEFAULT_NAV_VISIBLE, DEFAULT_NAV_HIDDEN);
+        (_c = (_b = this._navigatorQuery) === null || _b === void 0 ? void 0 : _b.classList) === null || _c === void 0 ? void 0 : _c.replace(DEFAULT_NAV_HIDDEN, DEFAULT_NAV_VISIBLE);
+    };
+    return Controller;
+}());
+var main = document.querySelector("#main-toggle");
+var humanizer = document.querySelector("#humanizer-toggle");
+var weapon = document.querySelector("#weapon-toggle");
+var triggerbot = document.querySelector("#triggerbot-toggle");
+var sound = document.querySelector("#sound-toggle");
+var tf2 = document.querySelector("#tf2-toggle");
+main.onclick = function () {
+    new Controller('#main', '#main-toggle');
+};
+humanizer.onclick = function () {
+    new Controller('#humanizer', '#humanizer-toggle');
+};
+weapon.onclick = function () {
+    new Controller('#weapon', '#weapon-toggle');
+};
+triggerbot.onclick = function () {
+    new Controller('#triggerbot', '#triggerbot-toggle');
+};
+sound.onclick = function () {
+    new Controller('#sound', '#sound-toggle');
+};
+tf2.onclick = function () {
+    new Controller('#tf2', '#tf2-toggle');
+};
